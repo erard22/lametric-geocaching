f633611272b166434a4ec01024e01841e68a83ad
Michel Erard <michel.erard@gmail.com>
2020-05-17 20:47:22 +0200

    added dummy test

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 const port = 3000;
 
@@ -17,7 +16,7 @@ app.get('/geocaching', (req, res) => {
 });
 
 app.get('/privacy.html', (req, res) => {
-    res.sendFile(path.join(__dirname + '/privacy.html'));
+    res.sendFile(__dirname + '/public/privacy.html');
 });
 
 
